0b700a02b85c2a8d22dd1ecbc021504338fedcdd
Łukasz Barkowski <lukasz.barkowski98@gmail.com>
2020-04-07 23:04:43 +0200

    change

--- rfid/test.py
+++ rfid/test.py
@@ -4,6 +4,8 @@ from pirc522 import RFID
 GPIO.setmode(GPIO.BOARD)
 
 rdr = RFID()
+util = rdr.util()
+
 print("Start")
 while True:
     rdr.wait_for_tag()
@@ -11,13 +13,10 @@ while True:
     if not error:
         (error, uid) = rdr.anticoll()
         if not error:
-            print("UID: " + str(uid))
-            # Select Tag is required before Auth
+            util.dump()
             if not rdr.select_tag(uid):
-                # Auth for block 10 (block 2 of sector 2) using default shipping key A
                 if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
                     # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-                    print("Reading block 10: " + str(rdr.read(10)))
                     # Always stop crypto1 when done working
                     rdr.stop_crypto()
 
